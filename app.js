0911a6c8b2e274672240b791b1f75ee5abab2e70
Matheus <Binotto>
2019-06-01 14:51:23 -0300

    Using Require

--- app.js
+++ app.js
@@ -1,70 +1,10 @@
-const chalk = require('chalk')
-const yargs = require('yargs')
-const notes = require('./notes.js')
+//Chamada da função FileSystem
+const fs = require('fs')
 
-// Customize yargs version
-yargs.version('1.1.0')
+//Chamada do método da função FileSystem, escreverá o texto abaixo "My name is Matheus"
+//no arquivo "notes.txt".
+fs.writeFileSync('notes.txt', 'My name is Matheus.')
 
-// Create add command
-yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        },
-        body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler(argv) {
-        notes.addNote(argv.title, argv.body)
-    }
-})
-
-// Create remove command
-yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler(argv) {
-        notes.removeNote(argv.title)
-    }
-})
-
-// Create list command
-yargs.command({
-    command: 'list',
-    describe: 'List your notes',
-    handler() {
-        notes.listNotes()
-    }
-})
-
-// Create read command
-yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    builder: {
-        title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
-        }
-    },
-    handler(argv) {
-        notes.readNote(argv.title)
-    }
-})
-
-yargs.parse()
+//Chamada do método da função FileSystem, diferente do que está descrito acima,
+//esse método acrescentará texto no arquivo "notes.txt".
+fs.appendFileSync('notes.txt', 'Curitiba')
